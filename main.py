a3442e30c39476c26dac4e5ae18084480fcb8fa4
João <joaopioonze@gmail.com>
2026-03-18 19:02:38 -0300

    Realizando data cleaning II

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # %% [markdown] 
-# Importando Bibliotecas
+## Importando Bibliotecas
 
 # %%
 import pandas as pd
@@ -8,12 +8,44 @@ import seaborn as sns
 import os
 
 # %% [markdown]
-# Importando Dados
+## Importando Dados
 
 # %%
-df = pd.read_csv('data/olist_customers_dataset.csv')
-print(f'{df.isna().count()}, {df.shape}')
-
+custumers = pd.read_csv("data\olist_customers_dataset.csv")
+orders = pd.read_csv("data\olist_orders_dataset.csv")
+order_items = pd.read_csv("data\olist_order_items_dataset.csv")
+order_payments = pd.read_csv("data\olist_order_payments_dataset.csv")
+order_review = pd.read_csv("data\olist_order_reviews_dataset.csv")
+products = pd.read_csv("data\olist_products_dataset.csv")
+sellers = pd.read_csv("data\olist_sellers_dataset.csv")
+category_name = pd.read_csv("data\product_category_name_translation.csv")
 
 
 # %%
+
+custumers[['customer_city', 
+           'customer_state']] = custumers[['customer_city', 
+                                           'customer_state']].astype('category')
+
+# %%
+
+# nao mechi em orders
+
+# %%
+order_items['order_item_id'] = order_items['order_item_id'].astype('category')
+
+# %%
+
+order_payments['payment_type'] = order_payments['payment_type'].astype('category')
+order_payments['payment_sequential'] = order_payments['payment_sequential'].astype('category')
+order_payments['payment_installments'] = order_payments['payment_installments'].astype('category')
+
+# %%
+# nao mechi em products
+# %%
+
+sellers['seller_city'] = sellers['seller_city'].astype('category')
+sellers['seller_state'] = sellers['seller_state'].astype('category')
+
+# %%
+
