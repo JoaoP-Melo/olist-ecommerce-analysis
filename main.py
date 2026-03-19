6077f7a364266ce3dacd7e4d86651f65d7235c40
João <joaopioonze@gmail.com>
2026-03-19 18:05:32 -0300

    Começando a analisar, pergunta 01

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
-# %% [markdown] 
-## Importando Bibliotecas
-
 # %%
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-import os
 
-# %% [markdown]
-## Importando Dados
-
-# %%
 custumers = pd.read_csv("data\olist_customers_dataset.csv")
 orders = pd.read_csv("data\olist_orders_dataset.csv")
 order_items = pd.read_csv("data\olist_order_items_dataset.csv")
@@ -23,29 +15,23 @@ category_name = pd.read_csv("data\product_category_name_translation.csv")
 
 # %%
 
-custumers[['customer_city', 
-           'customer_state']] = custumers[['customer_city', 
-                                           'customer_state']].astype('category')
+order_items_analise = order_items[["order_id", "product_id"]]
+products_analise = products[["product_id","product_category_name"]]
+orders_analise = orders[["order_id", "customer_id"]]
+custumers_analise = custumers[["customer_id", "customer_state"]]
 
+df_analise = orders_analise.merge(right=custumers_analise, on="customer_id", )
+df_analise = df_analise.merge(right= order_items_analise, on = "order_id")
+df_analise = df_analise.merge(right=products_analise, on="product_id")
+
+df_analise = (df_analise[["customer_state", "product_category_name", "product_id"]]
+              .groupby(["customer_state", "product_category_name"], as_index=False)
+              .count())
+
+df_analise = df_analise.rename(columns={"product_id" : "qtd"})
+
+def maior(row):
+    return row[['product_category_name', 'qtd']].max()
+
+df_analise.groupby(["customer_state"]).apply(maior)
 # %%
-
-# nao mechi em orders
-
-# %%
-order_items['order_item_id'] = order_items['order_item_id'].astype('category')
-
-# %%
-
-order_payments['payment_type'] = order_payments['payment_type'].astype('category')
-order_payments['payment_sequential'] = order_payments['payment_sequential'].astype('category')
-order_payments['payment_installments'] = order_payments['payment_installments'].astype('category')
-
-# %%
-# nao mechi em products
-# %%
-
-sellers['seller_city'] = sellers['seller_city'].astype('category')
-sellers['seller_state'] = sellers['seller_state'].astype('category')
-
-# %%
-
